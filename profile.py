bc2c063da839699fc4973c1a0c9e5444b4f9d8d1
Matthew Harrigan <harrigan@stanford.edu>
2017-05-15 12:23:06 -0700

    Profile

---
+++ profile.py
@@ -0,0 +1,20 @@
+import mdtraj as md
+import tensorflow as tf
+import tftraj.rmsd_op
+import mdtraj as md
+import numpy as np
+import pytest
+import timeit
+
+results = {}
+sess = tf.Session()
+traj = md.load(['fs_peptide/trajectory-{}.xtc'.format(i+1) for i in range(28)], top='fs_peptide/fs-peptide.pdb')
+traj = traj[::100]
+print(len(traj))
+rmsd = tftraj.rmsd_op.load()
+prmsd = rmsd.pairwise_msd(traj.xyz, traj.xyz)
+results['tf-cpu'] = timeit.timeit('sess.run(prmsd)', number=2, globals=globals())
+results['mdtraj'] = timeit.timeit('[md.rmsd(traj, traj, i) ** 2 for i in range(traj.n_frames)]', number=2, globals=globals())
+
+for k in sorted(results):
+    print("{:10s} {:5.2f}".format(k, results[k]))
