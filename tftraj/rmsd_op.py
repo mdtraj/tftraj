b871f1a287b45582e5ad4b22a6c2f44ce747168e
Matthew Harrigan <harrigan@stanford.edu>
2017-05-15 12:54:22 -0700

    Different release and debug so files

--- tftraj/rmsd_op.py
+++ tftraj/rmsd_op.py
@@ -1,8 +1,16 @@
 import tensorflow as tf
 import pkg_resources
+import warnings
+import os
 
 
 def load():
-    so = pkg_resources.resource_filename(__name__, 'rmsd/librmsd.so')
+    so = pkg_resources.resource_filename(__name__, 'rmsd/librmsd.Release.so')
+    if not os.path.isfile(so):
+        so = pkg_resources.resource_filename(__name__, 'rmsd/librmsd.Debug.so')
+        if not os.path.isfile(so):
+            raise FileNotFoundError("Could not find the RMSD op shared library. "
+                                    "Make sure you compile it!")
+        warnings.warn("Using debug build of RMSD Op. This will be slow!")
     mod = tf.load_op_library(so)
     return mod
