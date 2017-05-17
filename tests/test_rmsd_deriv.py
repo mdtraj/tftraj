Fix gradient w.r.t. first

tests/test_rmsd_deriv.py
@@ -6,7 +6,7 @@ import tftraj.rmsd
 import tftraj.rmsd_op
 
 
-def test_gradients(sess):
+def test_works(sess):
     traj = md.load('fip35.500.xtc', top='fip35.pdb')
     rmsd = tftraj.rmsd_op.load()
     inds = [5, 19, 234, 235]
@@ -23,7 +23,7 @@ def test_gradients(sess):
     print(grad_result)
 
 
-def test_gradients_vs_tensorflow(sess):
+def test_vs_tensorflow_target(sess):
     traj = md.load('fip35.500.xtc', top='fip35.pdb')
     rmsd = tftraj.rmsd_op.load()
     inds = [5, 19, 234, 235]
@@ -40,3 +40,67 @@ def test_gradients_vs_tensorflow(sess):
     grad_from_tf = sess.run(grad_from_tf)
 
     np.testing.assert_almost_equal(grad_from_op, grad_from_tf, decimal=3)
+
+
+def test_vs_tensorflow_target_few_atoms(sess):
+    traj = md.load('fip35.500.xtc', top='fip35.pdb')
+    traj.atom_slice(np.arange(traj.n_atoms)[::10])
+    rmsd = tftraj.rmsd_op.load()
+    inds = [5, 19, 234, 235]
+    target = np.array(traj.xyz[inds])
+    target = tf.Variable(target)
+    sess.run(tf.global_variables_initializer())
+
+    rmsd_from_op, _ = rmsd.pairwise_msd(tf.constant(traj.xyz), target)
+    grad_from_op = tf.gradients(rmsd_from_op, [target])[0]
+    grad_from_op = sess.run(grad_from_op)
+
+    rmsd_from_tf = tftraj.rmsd.pairwise_msd(tf.constant(traj.xyz), target)
+    grad_from_tf = tf.gradients(rmsd_from_tf, [target])[0]
+    grad_from_tf = sess.run(grad_from_tf)
+
+    np.testing.assert_almost_equal(grad_from_op, grad_from_tf, decimal=3)
+
+
+def test_vs_tensorflow_traj(sess):
+    traj = md.load('fip35.500.xtc', top='fip35.pdb')
+    rmsd = tftraj.rmsd_op.load()
+    inds = [5, 19, 234, 235]
+    target = np.array(traj.xyz[inds])
+    target = tf.constant(target)
+    traj = tf.Variable(traj.xyz)
+    sess.run(tf.global_variables_initializer())
+
+    rmsd_from_op, _ = rmsd.pairwise_msd(traj, target)
+    grad_from_op = tf.gradients(rmsd_from_op, [traj])[0]
+    grad_from_op = sess.run(grad_from_op)
+
+    rmsd_from_tf = tftraj.rmsd.pairwise_msd(traj, target)
+    grad_from_tf = tf.gradients(rmsd_from_tf, [traj])[0]
+    grad_from_tf = sess.run(grad_from_tf)
+
+    np.testing.assert_almost_equal(grad_from_op, grad_from_tf, decimal=3)
+
+
+def test_vs_tensorflow_both_input(sess):
+    traj = md.load('fip35.500.xtc', top='fip35.pdb')
+    rmsd = tftraj.rmsd_op.load()
+    inds = [5, 19, 234, 235]
+    target = np.array(traj.xyz[inds])
+    traj = tf.Variable(traj.xyz)
+    target = tf.Variable(target)
+    sess.run(tf.global_variables_initializer())
+
+    rmsd_from_op, _ = rmsd.pairwise_msd(traj, target)
+    grad_from_op = tf.gradients(rmsd_from_op, [traj, target])
+    grad_from_op = sess.run(grad_from_op)
+
+    rmsd_from_tf = tftraj.rmsd.pairwise_msd(traj, target)
+    grad_from_tf = tf.gradients(rmsd_from_tf, [traj, target])
+    grad_from_tf = sess.run(grad_from_tf)
+
+    assert len(grad_from_tf) == 2
+    assert len(grad_from_op) == 2
+    for i in range(2):
+        print(i)
+        np.testing.assert_almost_equal(grad_from_op[i], grad_from_tf[i], decimal=3)

tftraj/rmsd_op.py
@@ -36,10 +36,10 @@ def _pairwise_msd_grad(op, grad, rot_grad):
     grad = tf.expand_dims(tf.expand_dims(grad, axis=-1), axis=-1)
 
     rots = op.outputs[1]
-    dxy = grad * (confs1 - tf.matmul(big_confs2, rots))
+    dxy = grad * (confs1 - tf.matmul(big_confs2, rots, transpose_b=True))
     sum_dxy = 2 * tf.reduce_sum(dxy, axis=1) / n_atom
 
-    dyx = grad * (confs2 - tf.matmul(big_confs1, rots))
+    dyx = grad * (confs2 - tf.matmul(big_confs1, rots, transpose_b=False))
     sum_dyx = 2 * tf.reduce_sum(dyx, axis=0) / n_atom
 
     return sum_dxy, sum_dyx